af913fe53ca55436bd05ceedfad3d5e694c4dca1
Pablo Moraga <pablo.moraga.aedo@gmail.com>
2017-11-15 16:07:03 -0300

    NativeScript Angular

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,33 +1,41 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
+// import {CUSTOM_PROVIDERS} from "nativescript-angular/router";
+
+// nativeScriptBootstrap(AppComponent, [CUSTOM_PROVIDERS], { startPageActionBarHidden: false });
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
+
 import { AppRoutingModule } from "./app.routing";
 import { AppComponent } from "./app.component";
+import { MenuComponent } from "./menu/menu.component";
 
-import { ItemService } from "./item/item.service";
-import { ItemsComponent } from "./item/items.component";
-import { ItemDetailComponent } from "./item/item-detail.component";
+import { DishService } from './services/dish.service';
+import { ProcessHTTPMsgService } from './services/process-httpmsg.service';
+
+import { baseURL } from './shared/baseurl';
 
 // Uncomment and add to NgModule imports if you need to use two-way binding
 // import { NativeScriptFormsModule } from "nativescript-angular/forms";
 
 // Uncomment and add to NgModule imports  if you need to use the HTTP wrapper
-// import { NativeScriptHttpModule } from "nativescript-angular/http";
+ import { NativeScriptHttpModule } from "nativescript-angular/http";
 
-@NgModule({
+ @NgModule({
     bootstrap: [
         AppComponent
     ],
     imports: [
         NativeScriptModule,
-        AppRoutingModule
+        AppRoutingModule,
+        NativeScriptHttpModule
     ],
     declarations: [
         AppComponent,
-        ItemsComponent,
-        ItemDetailComponent
+        MenuComponent
     ],
     providers: [
-        ItemService
+        {provide: 'BaseURL', useValue: baseURL},
+        DishService,
+        ProcessHTTPMsgService
     ],
     schemas: [
         NO_ERRORS_SCHEMA
@@ -37,3 +45,6 @@ import { ItemDetailComponent } from "./item/item-detail.component";
 Pass your application module to the bootstrapModule function located in main.ts to start your app
 */
 export class AppModule { }
+
+
+//nativeScriptBootstrap(AppComponent, [CUSTOM_PROVIDERS], { startPageActionBarHidden: false });
