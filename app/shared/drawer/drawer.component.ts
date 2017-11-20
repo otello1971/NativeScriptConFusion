8cb56c43b806c02067af9c615b7dde92986502e0
Pablo Moraga <pablo.moraga.aedo@gmail.com>
2017-11-20 13:21:47 -0300

    fonts 2

--- app/shared/drawer/drawer.component.ts
+++ app/shared/drawer/drawer.component.ts
@@ -4,6 +4,7 @@ import { TNSFontIconService } from 'nativescript-ngx-fonticon';
 @Component({
     selector: 'drawer-content',
     templateUrl: './shared/drawer/drawer.component.html',
+    styleUrls: ['./shared/drawer/drawer.component.css']
 })
 export class DrawerComponent {
     constructor(private fonticon: TNSFontIconService) {
